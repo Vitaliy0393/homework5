1cb0178c404205239cd4e12d98eb5278d77290c3
Vitaliy0393 <167925531+Vitaliy0393@users.noreply.github.com>
2024-05-09 22:17:32 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -2,10 +2,10 @@ my_list = ['orange', 'apple', 'banaba', 'coconut']
 print(my_list)
 print(my_list [0])
 print(my_list [-1])
+print(my_list[2:5])
 my_list[2]='strawbery'
 print(my_list)
 my_dict = {'orange': 'апельсин', 'apple': 'яблоко', 'banana': 'банан', 'coconut': 'кокос'}
 print(my_dict)
-print(my_dict.values())
 my_dict = {'orange': ['апельсин', 'мандарин'], 'apple': 'яблоко', 'banana': 'банан', 'coconut': 'кокос'}
-print(my_dict)
+print(my_dict['orange'])
